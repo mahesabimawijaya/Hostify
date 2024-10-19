ab4dd69c63345fe4fa41660bc417c600d0a3b16c
astnab <astnab@gmail.com>
2024-10-19 16:48:26 +0700

    fix: minor fix ui dashboard

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,10 +4,17 @@ import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import HeroSection from "@/components/section/HeroSection";
 import PricingSection from "@/components/section/PricingSection";
+import ServiceSection from "@/components/section/ServiceSection";
 import WhyUsSection from "@/components/section/WhyUsSection";
 import Loading from "@/components/ui/Loading";
 import { fetchData } from "@/lib/axios";
-import { IHeroSection, IPricingSection, IWhyUsSection, PurpleAttributes } from "@/types/asset";
+import {
+  IHeroSection,
+  IPricingSection,
+  IServiceSection,
+  IWhyUsSection,
+  PurpleAttributes,
+} from "@/types/asset";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -35,7 +42,12 @@ export default function Home() {
       <Navbar />
       <HeroSection heroSection={asset?.heroSection as IHeroSection} />
       <WhyUsSection whyUsSection={asset?.whyUsSection as IWhyUsSection} />
-      <PricingSection pricingSection={asset?.pricingSection as IPricingSection} />
+      <PricingSection
+        pricingSection={asset?.pricingSection as IPricingSection}
+      />
+      <ServiceSection
+        serviceSection={asset?.serviceSection as IServiceSection}
+      />
       <Footer />
     </>
   );
--- web/src/app/profile/page.tsx
+++ web/src/app/profile/page.tsx
@@ -34,7 +34,10 @@ const Profile = () => {
     if (loggedinUser.id) {
       const fetchTransactions = async () => {
         try {
-          const data = await fetchData("api", `transaction/user/${loggedinUser.id}?productId=${productId}`);
+          const data = await fetchData(
+            "api",
+            `transaction/user/${loggedinUser.id}?productId=${productId}`
+          );
           setTransactions(data.data);
         } catch (error) {
           throw new Error(error as string);
@@ -60,31 +63,40 @@ const Profile = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <section className="bg-gray-100  p-3 sm:p-5 xl:pb-[300px]">
+    <section className="bg-[#F7F3FF]  p-3 sm:p-5 xl:pb-[300px] h-screen">
       <div className="mx-auto max-w-screen-xl px-4 lg:px-12 pt-[10px]">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl mb-5 mt-10">Your Profile</h1>
-          <button onClick={handleLogout} className="px-5 py-2 bg-red-600 text-white rounded hover:opacity-80">
+          <button
+            onClick={handleLogout}
+            className="px-5 py-2 bg-red-600 text-white rounded hover:opacity-80 lg:hidden"
+          >
             Logout
           </button>
         </div>
-        <hr />
-        <div className="flex items-center mt-5 bg-white p-6 rounded-full shadow-lg">
+
+        <div className="flex items-center mt-5 bg-white p-6 rounded-full border border-zinc-200">
           <div className="relative w-20 h-20">
             <Image src={"/profile-user.png"} fill alt="profile" />
           </div>
           <div className="flex flex-col ml-4">
-            <h3 className="text-xl font-semibold">{loggedinUser.firstName + " " + loggedinUser.lastName}</h3>
+            <h3 className="text-xl font-semibold">
+              {loggedinUser.firstName + " " + loggedinUser.lastName}
+            </h3>
             <h4 className="text-gray-500">{loggedinUser.email}</h4>
           </div>
         </div>
-        <h1 className="text-3xl mb-5 mt-14">Your Transaction History</h1>
-        <hr />
+
+        <h1 className="font-semibold text-xl mb-5 mt-14">
+          Your transaction history
+        </h1>
+
         {/* start coding here */}
-        <div className="bg-white  relative shadow-xl sm:rounded-lg overflow-hidden mt-5">
+        <div className="bg-white  relative border border-zinc-200 sm:rounded-lg overflow-hidden mt-5">
           <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
             <div className="w-full md:w-1/2">
-              <h2 className="text-2xl font-semibold text-primary">Transaction</h2>
+              <h2 className="text-2xl font-semibold text-primary">
+                Transaction
+              </h2>
             </div>
             <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
               <div className="flex items-center w-full md:w-auto">
@@ -98,7 +110,13 @@ const Profile = () => {
                     setIsActive(!isActive);
                   }}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="h-4 w-4 mr-2 text-primary" viewBox="0 0 20 20" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="h-4 w-4 mr-2 text-primary"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
                     <path
                       fillRule="evenodd"
                       d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
@@ -106,15 +124,32 @@ const Profile = () => {
                     />
                   </svg>
                   Filter by Product
-                  <svg className="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clipRule="evenodd" fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+                  <svg
+                    className="-mr-1 ml-1.5 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    />
                   </svg>
                 </button>
                 {/* filter dropdown */}
                 {isActive && (
-                  <div id="filter-dropdown" className="absolute bg-white rounded border shadow-md top-[60px] right-4">
+                  <div
+                    id="filter-dropdown"
+                    className="absolute bg-white rounded border shadow-md top-[60px] right-4"
+                  >
                     {products.map((product, i) => (
-                      <Link scroll={false} key={i} href={`?product=${product.id}`}>
+                      <Link
+                        scroll={false}
+                        key={i}
+                        href={`?product=${product.id}`}
+                      >
                         <p
                           onClick={() => {
                             setIsActive(false);
@@ -157,16 +192,24 @@ const Profile = () => {
               <tbody>
                 {transactions.map((transaction, i) => (
                   <tr className="border-b " key={i}>
-                    <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap capitalize">
+                    <th
+                      scope="row"
+                      className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap capitalize"
+                    >
                       {transaction.product.name}
                     </th>
                     <td className="px-4 py-3">
-                      {transaction.term} {transaction.term > 1 ? "months" : "month"}
+                      {transaction.term}{" "}
+                      {transaction.term > 1 ? "months" : "month"}
                     </td>
-                    <td className="px-4 py-3">{toRupiah(transaction.amount)}</td>
+                    <td className="px-4 py-3">
+                      {toRupiah(transaction.amount)}
+                    </td>
                     <td className="px-4 py-3">{transaction.paymentStatus}</td>
                     <td className="px-4 py-3">{transaction.paymentMethod}</td>
-                    <td className="px-4 py-3">{new Date(transaction.createdAt).toDateString()}</td>
+                    <td className="px-4 py-3">
+                      {new Date(transaction.createdAt).toDateString()}
+                    </td>
                   </tr>
                 ))}
               </tbody>
